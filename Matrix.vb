4c5e5161b92856af5ef006f92379eafbfc03bc21
Ranieri <ran-junior@hotmail.com>
2018-09-18 14:04:38 -0300

    Create Matrix.vb

---
+++ Matrix.vb
@@ -0,0 +1,26 @@
+Public Class Matrix
+    Private rows, cols As Integer
+    Private rnd As New Random()
+    Public data
+
+    Sub New(rows, cols)
+        Me.rows = rows
+        Me.cols = cols
+
+        'Initializing Matrix and Fill Matrix
+        Dim numbers(rows - 1, cols - 1) As Integer
+        Me.data = numbers
+    End Sub
+
+    'Other Stuff
+    Public Sub randomize()
+
+    End Sub
+
+
+    Public Function map(m1 As Matrix, func As Object)
+        Dim matrix = New Matrix(m1.rows, m1.cols)
+        matrix.data = m1.data.Select(Function(v) v.Select(Function(x) x).ToArray()).ToArray()
+        Return matrix
+    End Function
+End Class
