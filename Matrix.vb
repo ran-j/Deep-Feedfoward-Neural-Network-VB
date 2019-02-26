2cadd01c15e9cd6731845a8f1254b342cbf98637
Ranieri Abreu Silva Junior <rabreu@indracompany.com>
2019-02-26 14:58:27 -0300

    Add functions

--- Matrix.vb
+++ Matrix.vb
@@ -1,25 +1,43 @@
 Public Class Matrix
-    Private rows, cols As Integer
+    Public rows, cols As Integer
     Private ReadOnly rnd As New Random()
     Public data
 
     Sub New(rows, cols)
         Me.rows = rows
         Me.cols = cols
-
         'Initializing Matrix and Fill Matrix
-        Dim numbers(rows - 1, cols - 1) As Integer
-        Me.data = numbers
+        Me.data = MatrixCreate(rows, cols)
     End Sub
 
-    'Other Stuff
-    Public Sub Randomize()
+    Public Function map(matrix As Matrix)
+        Dim mtx = New Matrix(matrix.rows, matrix.cols)
 
-    End Sub
-
-    Public Function Map(m1 As Matrix, func As Object)
-        Dim matrix = New Matrix(m1.rows, m1.cols)
-        matrix.data = m1.data.Select(Function(v) v.Select(Function(x) x).ToArray()).ToArray()
-        Return matrix
+        Return mtx
     End Function
+
+    Public Function FromArray(arr)
+        Return New Matrix(arr.Count, 1)
+    End Function
+
+    Public Function Copy() As Matrix
+        Return New Matrix(rows, cols) With {
+            .data = MatrixCreate(rows, cols)
+        }
+    End Function
+
+    Private Function MatrixCreate(ByVal rows As Integer, ByVal cols As Integer) As Double()()
+
+        Dim result(0 To rows - 1)() As Double
+        Dim i As Integer
+        For i = 0 To rows - 1
+            Dim col(0 To cols - 1) As Double
+            result(i) = col
+        Next i
+
+        Return result
+
+    End Function
+
 End Class
+
